a75f7614c5a8e75ccf5c08ca7983db7878f616bc
bhaas412 <bhaas412@gmail.com>
2022-08-23 19:30:47 -0400

    Renamed post_id to review_id in Comment model

--- models/Comment.js
+++ models/Comment.js
@@ -27,10 +27,10 @@ Comment.init(
                 key: 'id'
             }
         },
-        post_id: {
+        review_id: {
             type: DataTypes.INTEGER,
             references: {
-                model: 'post',
+                model: 'review',
                 key: 'id'
             }
         }
