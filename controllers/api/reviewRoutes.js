dc893585fdbd3ae64a3448ed982305f3f1c29e61
bhaas412 <bhaas412@gmail.com>
2022-08-23 19:22:06 -0400

    Renamed Post routes to Review routes to correspond with DB tables. Modified models and API routes to correspond with DB as well

---
+++ controllers/api/reviewRoutes.js
@@ -0,0 +1,151 @@
+const router = require('express').Router();
+const { Review, User, Comment } = require('../../models');
+const sequelize = require('../../config/connection');
+const withAuth = require('../../utils/auth');
+
+// route to all reviews
+router.get('/', (req, res) => {
+    Review.findAll({
+        attributes: [
+            'id',
+            'title',
+            'review_body',
+            'created_at'
+        ],
+        include: [
+            { 
+                model: User,
+                attributes: ['username'] 
+            },
+            {
+                model: Comment,
+                attributes: [
+                    'id',
+                    'comment_text',
+                    'review_id',
+                    'user_id',
+                    'created_at'
+                ],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            }
+        ],
+        order: [['created_at', 'DESC']]
+    })
+    .then(data => res.json(data))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+
+// retrieve single review
+router.get('/:id', (req, res) => {
+    Review.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'title',
+            'review_body',
+            'created_at'
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }, 
+            {
+                model: Comment,
+                attributes: [
+                    'id',
+                    'comment_text',
+                    'review_id',
+                    'user_id',
+                    'created_at'
+                ],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            }
+        ]
+    })
+    .then(data => {
+        if (!data) {
+            res.status(404).json({ message: 'No review found with this id number!' });
+            return;
+        }
+        res.json(data);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// create reviews
+router.review('/', withAuth, (req, res) => {
+    Review.create({
+        title: req.body.title,
+        review_body: req.body.review_body,
+        user_id: req.session.user_id
+    })
+    .then(data => res.json(data))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// edit personal review
+router.put('/:id', withAuth, (req, res) => {
+    Review.update(
+        {
+            title: req.body.title,
+            review_body: req.body.review_body
+        },
+        {
+            where: {
+                id: req.params.id
+            }
+        }
+    )
+    .then(data => {
+        if (!data) {
+            res.status(404).json({ message: 'No review found with this id number! '});
+            return;
+        }
+        res.json(data);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// delete review 
+router.delete('/:id', withAuth, (req, res) => {
+    Review.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(data => {
+        if (!data) {
+            res.status(404).json({ message: 'No review found with this id number!' });
+            return;
+        }
+        res.json(data);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
---
+++ models/Review.js
@@ -0,0 +1,45 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Review extends Model {}
+
+// TODO: Add additional keys to Review table
+
+Review.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    location: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'review',
+  }
+);
+
+module.exports = Review;
