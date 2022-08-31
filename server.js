b0479a3c7ad2ce611934ea62e64dc4e372bbfb50
bhaas412 <bhaas412@gmail.com>
2022-08-30 22:40:13 -0400

    Reconfigured Tailwind CSS stuff to properly style the homepage on our web server deployed locally

--- server.js
+++ server.js
@@ -39,6 +39,7 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.use(routes);
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit',
-    purge: [
-      './html/**.{html,js}'
+    content: [
+      './views/**.{handlebars, html, js}'
     ],
   theme: {
     colors: {
