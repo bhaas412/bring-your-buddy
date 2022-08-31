5a9357311b4103715283800116ead207e5314374
jaquel <jaquelrey@gmail.com>
2022-08-30 20:49:31 -0400

    Responsive html and custom tailwind config

--- html/home.js
+++ html/home.js
@@ -1,7 +1,31 @@
 
-const btn = document.querySelector('.mobile-menuBtn')
-const menu = document.querySelector('.mobile-menu')
+const btn = document.getElementById('mobilebtn')
+const menu = document.getElementById('mobilemenu')
+const searchbar = document.getElementById('searchbar')
+const select = document.getElementById('typeselect')
+let query = []
 
 btn.addEventListener('click', () => {
-menu.classList.toggle('hidden')
+    menu.classList.toggle('hidden')
 });
+
+//getting data from dropdown and search element to pass as query
+
+function selectedL(event){
+    
+    console.log(locationType)
+}
+
+function runsearch(){
+    const locationQuery = searchbar.value
+    const locationType = typeselect.value
+
+    if(locationType !== "Type..") {
+        query.push(locationType)
+    }
+
+    query.push(locationQuery)
+    
+
+    console.log(query)
+}
