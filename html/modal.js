eb1392686c7e66d31d6a3f04fb637b73e7ba6790
jaquel <jaquelrey@gmail.com>
2022-09-01 16:14:02 -0400

    not as broken

--- html/modal.js
+++ html/modal.js
@@ -34,10 +34,32 @@ moblileSignupbtn.addEventListener('click', function() {
 
 
 
-//modal close buttons
+//login logout modal close buttons
 loginxbutton.addEventListener('click', function() {
     loginmodal.style.display = "none";
 })
 signupxbutton.addEventListener('click', function() {
     signupmodal.style.display = "none";
 })
+
+
+//post and comment modal
+let postmodal = document.getElementById("post-modal")
+let commentmodal = document.getElementById("comment-modal")
+let postxbutton = document.getElementById("post-x-btn");
+let commentxbutton = document.getElementById("comment-x-btn");
+
+//modal close buttons
+postxbutton.addEventListener('click', function() {
+    postmodal.style.display = "none";
+})
+
+commentxbutton.addEventListener('click', function() {
+    commentmodal.style.display = "none";
+})
+
+
+
+
+
+
